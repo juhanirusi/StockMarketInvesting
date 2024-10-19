017e85b12589133302d3db824e97f5de69126349
Juhani Rusi <juhanirusi@yahoo.com>
2024-10-19 22:16:26 +0300

    Added path for stock-splits-and-dividends to the list.

--- utils/create_file_directories.py
+++ utils/create_file_directories.py
@@ -5,11 +5,13 @@ CWD = os.getcwd()  # Get the current working directory
 DATA_DIR = f"{CWD}/data"
 FINANCIAL_REPORTS_DIR = f"{DATA_DIR}/financial-reports"
 HISTORICAL_STOCK_PRICES_DIR = f"{DATA_DIR}/historical-stock-prices"
+STOCK_SPLITS_AND_DIVIDENDS_DIR = f"{DATA_DIR}/stock-splits-and-dividends"
 
 DIRECTORIES = [
     DATA_DIR,
     FINANCIAL_REPORTS_DIR,
     HISTORICAL_STOCK_PRICES_DIR,
+    STOCK_SPLITS_AND_DIVIDENDS_DIR,
 ]
 
 
