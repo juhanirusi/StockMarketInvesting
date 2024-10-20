9043e503ea3b92ff4b2449292f806f69828762c5
Juhani Rusi <juhanirusi@yahoo.com>
2024-10-20 13:07:59 +0300

    Deleted some code and simplified method calls.

--- run.py
+++ run.py
@@ -12,27 +12,6 @@ fetch_stock_data = FetchDataForStock()
 STOCK_TICKER = "ASML"
 
 
-# stock = yf.Ticker(STOCK_TICKER)
-# print(stock)
-
-# calendar = stock.get_calendar()  # --> Dividend dates, earnings date, EPS and Revenue estimates
-# info = stock.get_info()  # --> Useful info and different ratios
-# inst_holders = stock.get_institutional_holders() # --> See institutional holders/percentages/value
-# recommendations = stock.get_recommendations() # --> Get recommendations from analysts
-
-# # PRINT THE RESULTS
-# print(actions)
-# print(calendar)
-# print(info)
-# print(inst_holders)
-# print(recommendations)
-
-# print(company)
-
-# company.to_csv(
-#     r"C:\Users\juhan\OneDrive\Omat Tiedostot\GitHub\StockMarketInvesting\data\company.csv"
-# )
-
 if __name__ == "__main__":
 
     if not FMP_API_KEY:
@@ -46,7 +25,4 @@ if __name__ == "__main__":
             stock_ticker=STOCK_TICKER, financial_statement=financial_statement
         )
 
-    fetch_stock_data.fetch_historical_data_to_csv_file(stock_ticker=STOCK_TICKER)
-    fetch_stock_data.fetch_dividend_and_stock_split_data_to_csv_file(
-        stock_ticker=STOCK_TICKER
-    )
+    fetch_stock_data._fetch_stock_and_company_data(stock_ticker=STOCK_TICKER)
